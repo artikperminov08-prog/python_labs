8c7fd85c8b5e35ceb556965f479f3e3e46bd5f55
Артём Перминов <artikperminov08@gmail.com>
2026-09-22 23:53:14 +0300

    Лаба

--- src/lab01/ex05.py
+++ src/lab01/ex05.py
@@ -1,4 +1,4 @@
 fam, name, otch = [i for i in list(input("Фио: ").split())]
 leng = len(fam) + len(name) + len(otch) + 2
-print(f'Инициалы: {fam[0]}{name[0]}{otch[0]}')
+print(f'Инициалы: {fam[0].upper()}{name[0].upper()}{otch[0].upper()}')
 print(f'Длина (символов): {leng}')
